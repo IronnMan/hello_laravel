109780e2385ca06f3b4f407b403bc95412f6a7e0
wenlin <wenlin42@icloud.com>
2018-03-08 05:45:43 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,11 @@
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            <a href="{{ route('home') }}" id="logo">Laravel App</a>
+            <nav class="nav navbar-nav navbar-right">
+                <li><a href="{{ route('help') }}">帮助</a></li>
+                <li><a href="#">登录</a></li>
+            </nav>
+        </div>
+    </div>
+</header>
