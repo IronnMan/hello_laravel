2b52ec2dc8d9d6cd39a1322836efbf1d0d269cc3
wenlin <wenlin42@icloud.com>
2018-03-08 08:15:20 +0000

    应用 mdbootstrap

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,11 +1,28 @@
-<header class="navbar navbar-fixed-top navbar-inverse">
-    <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-            <a href="{{ route('home') }}" id="logo">Laravel App</a>
-            <nav class="nav navbar-nav navbar-right">
-                <li><a href="{{ route('help') }}">帮助</a></li>
-                <li><a href="#">登录</a></li>
-            </nav>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark black">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('home') }}"><strong>LaravelApp</strong></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+            <div class="collapse navbar-collapse navbar-right" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('help') }}">帮助 <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">登录</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
+    </nav>
+    {{--
+    <div class="col-md-offset-1 col-md-10">
+        <nav class="nav navbar-nav navbar-right">
+            <li><a href="{{ route('help') }}">帮助</a></li>
+            <li><a href="#">登录</a></li>
+        </nav>
+    </div> --}}
 </header>
